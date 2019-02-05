f174f665ddb2ac4a73f8afeebb8317eb1fa82b4e
T4mmi <thomas.janvier@univ-orleans.fr>
2019-02-05 11:35:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-thickness3d',
-    version='5.0b1',
+    version='5.0rc1',
     author='Insight Software Consortium',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
